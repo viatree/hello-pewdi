7b7902da56af08abf8bd4c09093a8bc519658a8b
viatree <viathree01@gmail.com>
2020-01-15 23:41:00 +0700

    Bersama Iki Test

---
+++ Result.java
@@ -0,0 +1,9 @@
+public class Test {
+    
+    public static void main(String[] args) {
+        System.out.println("welcome");
+        System.out.println("to");
+        System.out.println("the");
+        System.out.println("System");
+    }
+}
